802207ca5db73d7f0880aa0864a6f6bb283dfe44
paolapino <paolapino@unicauca.edu.co>
2019-08-31 09:49:08 -0500

    Primer commit

---
+++ src/main/java/com/springboot/storedprocedure/repository/Profesordao.java
@@ -0,0 +1,45 @@
+package com.springboot.storedprocedure.repository;
+
+
+import javax.persistence.EntityManager;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.springboot.storedprocedure.model.Centro_Educativo;
+import com.springboot.storedprocedure.model.Profesor;
+
+@Repository
+public class Profesordao {
+	@Autowired
+	private EntityManager em;
+
+	
+	@SuppressWarnings("unchecked")
+	public Iterable<Profesor> get_all_profesor() {
+		return em.createNamedStoredProcedureQuery("procedure-mostrar-profesor").getResultList();
+	}
+	
+	
+	public Integer insertar_profesor(Long prof_id,String prof_titulo,String prof_experiencia_laboral) {
+		return (Integer) em.createNamedStoredProcedureQuery("procedure-insertar-profesor")
+				.setParameter("id_prof", prof_id)
+				.setParameter("titulo_prof", prof_titulo)
+				.setParameter("experiencia_laboral_prof", prof_experiencia_laboral)
+				.getOutputParameterValue("ejecuto");
+	}
+	
+	public Integer actualizar_profesor(Long prof_id,String prof_titulo,String prof_experiencia_laboral) {
+		return (Integer) em.createNamedStoredProcedureQuery("procedure-actualizar-profesor")
+				.setParameter("id_prof", prof_id)
+				.setParameter("titulo_prof", prof_titulo)
+				.setParameter("experiencia_laboral_prof", prof_experiencia_laboral)
+				.getOutputParameterValue("ejecuto");
+	}
+	
+	public Integer eliminar_profesor(Long prof_id) {
+		return (Integer) em.createNamedStoredProcedureQuery("procedure-eliminar-profesor")
+				.setParameter("id_prof", prof_id)
+				.getOutputParameterValue("ejecuto");
+	}
+}
